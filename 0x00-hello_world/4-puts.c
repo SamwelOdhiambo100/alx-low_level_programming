d485cc06f5d5b5c7fc7f525248b74c900296ee8c
SamwelOdhiambo100 <smwldhmb@gmail.com>
2023-03-17 05:52:21 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * main - entry point
+ *   *   *
+ *    *    * This function prints a message to the console and returns 0 on success.
+ *     *     *
+ *      *      * Return: Always 0 (success)
+ *      */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
