6f855ca4ec5cd329fe8477a26611ceac23d41605
SamwelOdhiambo100 <smwldhmb@gmail.com>
2023-03-20 22:51:36 +0000

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,10 +4,11 @@
  *   * Description: print numbers
  *    *
  *     * Return: Always 0(sucess)
- *      */
+*/
 int main(void)
 {
 	int num;
+
 	for (num = 0; num < 10; num++)
 
 		putchar((num % 10) + '0');
